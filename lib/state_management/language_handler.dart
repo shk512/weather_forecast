3ee70ff2b2e5583e9e9f3d19e9e1374fa5fe7034
Mukaram Shk <mukarramshk512@gmail.com>
2023-11-27 09:08:22 +0500

    Optimize code and implement on page refresh

--- lib/state_management/language_handler.dart
+++ lib/state_management/language_handler.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:weather_forecast/services/spf.dart';
 
 class LanguageHandler extends ChangeNotifier {
   String languageCode = "en";
 
   void setLanguageCode(String code) {
     languageCode = code;
+    SpfService().saveLanguageCode(code);
     notifyListeners();
   }
 }
